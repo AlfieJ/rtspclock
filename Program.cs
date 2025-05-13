50d41e469f1c030dd2ded9a28acf69c11ba1a3b7
Alfred Johnson <aejohns@sandia.gov>
2025-05-13 14:17:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rtspclock
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var libVLC = new LibVLC(enableDebugLogs: true);
+            libVLC.Log += (obj, eventArgs) =>
+            {
+                Console.WriteLine("libVLC: " + eventArgs.Message);
+            };
+
+            var stream = new MemoryStream();
+            var streamMediaInput = new StreamMediaInput(stream);
+
+            int width = 1920;
+            int height = 1024;
+
+            var bitmap = new Bitmap(width, height);
+            var graphics = Graphics.FromImage(bitmap);
+            var font = new Font(FontFamily.GenericSansSerif, 24);
+
+            // Center horizontally and vertically
+            var strFormat = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+            var brush = new SolidBrush(Color.White);
+            var padding = new Rectangle(0, 0, width, height);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    graphics.Clear(Color.DarkGray);
+
+                    var dt = DateTime.Now.ToString("yyyy-MM-dd\nhh:mm:ss.fff");
+                    graphics.DrawString(dt, font, brush, padding, strFormat);
+
+                    bitmap.Save(stream, ImageFormat.Jpeg);
+
+                    Thread.Sleep(500);
+                }
+            });
+
+            var player = new MediaPlayer(libVLC)
+            {
+                EnableHardwareDecoding = true,
+            };
+            player.EncounteredError += (_, err) =>
+            {
+                Console.Error.WriteLine("MediaPlayer: " + err.ToString());
+            };
+
+            string[] options =
+            {
+                ":sout=#rtp{{sdp=rtsp://127.0.0.1:7000}}",
+                ":no-sout-all",
+                ":sout-keep",
+            };
+
+            var media = new Media(libVLC, streamMediaInput, options);
+
+            player.Play(media);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rtspclock")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sandia National Laboratories")]
+[assembly: AssemblyProduct("rtspclock")]
+[assembly: AssemblyCopyright("Copyright © Sandia National Laboratories 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc5b02b8-1095-4a83-ad63-3ede0aea4e5d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
